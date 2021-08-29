79e07caad09fd347effed432899c7e12603b3a1d
Sukriti Singh <singhsukriti94@gmail.com>
2021-08-29 19:56:41 -0400

    Server Commit

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 //allow cross-origin requests
 app.use(cors());
 
-mongoose.connect('mongodb+srv://sukriti94:sukriti1234@gql-project.phj7o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect('URL');
 mongoose.connection.once('open', () => {
     console.log('connected to database');
 })
